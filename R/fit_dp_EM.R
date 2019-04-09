6c9dd26c9394570d609c9753e76aceff242b5468
camponsah <charles.kamponsah@gmail.com>
2019-04-09 16:46:43 -0700

    Discrete Pareto EM algorithmn

--- R/fit_dp_EM.R
+++ R/fit_dp_EM.R
@@ -1,46 +1,45 @@
-#' 
-#' Algorithmn
-dpareto_em <- function(data,delta=1, p=0.5, maxiter=100, tol=1e-12)
-{
-   # initialization
-  N<-data
+
+
+dpareto_em <- function(data, delta = 1, p = 0.5, maxiter = 500, tol = 1e-12){
+  N<- data
   n <- length(N)
-  gamma1<- -1/(delta*log(1-p))
+  gamma_1<- - 1/(delta*log(1 - p))
   eta<- 1/delta
-  ##Likelihood function of Discrete Pareto
-  log_like<-function(N,gamma1, eta){
-    LL<- (1/(1+((N-1)/gamma1)))^(eta)-(1/(1+(N/gamma1)))^(eta)
+  # log-likelihood for discrete Pareto
+  log_like<- function(N, gamma_1, eta){
+  LL<- (1/(1 + ((N - 1)/gamma_1)))^(eta) - (1/(1 + (N/gamma_1)))^(eta)
     return(sum(log(LL)))
   }
-  ### function to solve for eta
+  # Function to optimize to eta
   func_eta<-function(eta){
-    ll<- eta*log(eta+tol)-lgamma(eta)-eta -eta*log(mean(a)+tol) +eta*mean(c)
-      #log(eta+tol)-digamma(eta)-log(a+tol)+c
-    return(-ll) 
+    ll<- eta*log(eta + tol) - lgamma(eta)- eta - eta*log(mean(a) + tol) + eta*mean(c)
+    return( - ll)
   }
-  Devianceold<-0
-  Deviancenew <- log_like(N,gamma1,eta)
-  Outi<-NULL;outd<-NULL;outp<-NULL;outD<-NULL; k=1 
-  Outi[1]<-0; outd[1]<-delta; outp[1]<-p; outD[1]<-Deviancenew
-  ##
-  while((abs(Deviancenew-Devianceold)>tol) & (k <= maxiter)){ 
+  # log-liklihood calcultion
+  Devianceold<- 0
+  Deviancenew <- log_like(N, gamma_1, eta)
+  # Intitialize vectors for storing data
+  Outi<- NULL; outd<- NULL; outp<-NULL; outD<- NULL; k = 1
+  Outi[1]<- 0; outd[1]<- delta; outp[1]<- p; outD[1]<- Deviancenew
+  while((abs(Deviancenew - Devianceold) > tol) & (k <= maxiter)){
     ### E step
-    const<- 1/(gamma(eta)*((gamma1+N-1)^(-eta) - (gamma1+N)^(-eta)))
-    a<- const * gamma(eta+1) * ((gamma1+N-1)^(-(eta+1)) - (gamma1+N)^(-(eta+1)))
-    t1<- digamma(eta)-log(gamma1+N-1)
-    t2<- digamma(eta)-log(gamma1+N)
-    c<-  const*gamma(eta) * (t1*(gamma1+N-1)^(-eta) -t2*(gamma1+N)^(-eta))
+    const<- 1/(gamma(eta)*((gamma_1 + N - 1)^(-eta) - (gamma_1 + N)^(-eta)))
+    a<- const * gamma(eta + 1) * ((gamma_1 + N - 1)^(-(eta + 1)) - (gamma_1 + N)^(-(eta + 1)))
+    t1<- digamma(eta) - log(gamma_1 + N - 1)
+    t2<- digamma(eta) - log(gamma_1 + N)
+    c<-  const*gamma(eta) * (t1*(gamma_1 + N - 1)^(-eta) - t2*(gamma_1 + N)^(-eta))
     #### M step
     eta<- stats:: nlm(f=func_eta,p=eta)$estimate
     delta<- 1/eta
-    p<-1-exp(-mean(a))
-    gamma1<- - 1/(delta*log(1-p))
+    p<- 1 - exp( - mean(a))
+    gamma_1<- - 1/(delta*log(1 - p))
     Devianceold<-Deviancenew
-    Deviancenew <- log_like(N,gamma1,eta)
+    Deviancenew <- log_like(N,gamma_1,eta)
     # Output
-    k=k+1
-    Outi[k]<-k; outd[k]<-delta; outp[k]<-p; outD[k]<-Deviancenew
+    k<- k + 1
+    Outi[k]<- k; outd[k]<- delta; outp[k]<- p; outD[k]<- Deviancenew
   }
+  #Output data
   Output <- data.frame(Outi,outd,outp,outD)
   names(Output) <- c("iteration","delta","p","log-lik values")
   result <- list(par=c(delta,p), Deviance=Deviancenew, data=Output)
